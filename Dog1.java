b1dfbcd88cb81e7871eeb5515671819bbc904a9a
Satya <smohapatra@shutterfly.com>
2016-02-03 13:50:17 -0800

    passing arguments to object methods - Dog1Driver.java

---
+++ Dog1.java
@@ -0,0 +1,19 @@
+public class Dog1 {
+	int size;
+	String name;
+
+	void bark (int size) {
+	
+	if (size < 10 ) {
+		System.out.println ("Ruff Ruff!!");
+
+	} else if ((size >=10) & (size <=50)) {
+		System.out.println ("Woof Woof!!");
+	} else {
+		System.out.println ("Wuff Wuff!!");
+	
+	}
+
+	}
+
+}
---
+++ Dog1Driver.java
@@ -0,0 +1,17 @@
+public class Dog1Driver {
+
+	public static void main (String[]  args) {
+	
+	Dog1 d1 = new Dog1();
+	Dog1 d2 = new Dog1();
+	Dog1 d3 = new Dog1();
+
+
+	d1.bark(3);
+	d2.bark(15);
+	d3.bark(60);
+	
+	
+	}
+
+}
