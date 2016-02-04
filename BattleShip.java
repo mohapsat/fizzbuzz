d15120cf2ab3ff074db102bbd08841af92fb08eb
Satya <smohapatra@shutterfly.com>
2016-02-04 15:31:22 -0800

    Simple BattleShips Game WIP

---
+++ BattleShip.java
@@ -0,0 +1,15 @@
+public class BattleShip {
+
+	private int userGuess = 0;
+	//private int [] ship = {'1','2','3'};
+	private	int [] gameGrid = {'1','2','3','4','5','6','7'};
+
+	public void setUserGuess (int num) {
+		userGuess = num;
+	}
+	
+	public int getUserGuess () {
+		return userGuess;
+	}
+
+}
---
+++ BattleShipTestDrive.java
@@ -0,0 +1,15 @@
+public class BattleShipTestDrive {
+
+	public static void main(String[] args) {
+	
+	BattleShip b = new BattleShip();
+
+	b.setUserGuess(2);
+	System.out.println ("User guess is: " + b.getUserGuess());
+
+	
+
+	
+	}
+
+}
