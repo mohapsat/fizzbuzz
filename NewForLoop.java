ff04877693deedca2de8d83840c3f5220b9a5da0
Satya <smohapatra@shutterfly.com>
2016-02-05 10:06:49 -0800

    added new

---
+++ NewForLoop.java
@@ -0,0 +1,21 @@
+public class NewForLoop {
+	
+	public static void main(String[] args) {
+	
+	String i[] = {"1","2","3","4","5"};
+
+
+	for (String j : i){
+	System.out.println (j);
+	
+	
+	}
+	
+	
+	
+	}
+
+
+
+
+}
---
+++ ParseInt.java
@@ -0,0 +1,12 @@
+public class ParseInt {
+
+	public static void main (String[] args) {
+	
+	String str = "1";
+
+	System.out.println (Integer.parseInt(str));
+		
+	
+	}
+
+}
